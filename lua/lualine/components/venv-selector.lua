a13e88d016ec272b5233c0e86633830e7533e814
tianyouzeng <tyzengty@gmail.com>
2023-12-20 00:19:16 +0800

    Automatically hide when not in python file

--- lua/lualine/components/venv-selector.lua
+++ lua/lualine/components/venv-selector.lua
@@ -1,22 +1,30 @@
 local M = require('lualine.component'):extend()
+local system = require('venv-selector.system')
 
 local default_opts = {
   icon = '',
   color = { fg = '#CDD6F4' },
+  auto_hide = true,
   on_click = function()
     require('venv-selector').open()
   end,
 }
 
 function M:init(options)
-  options = vim.tbl_deep_extend('keep', options or {}, default_opts)
   M.super.init(self, options)
+  self.options = vim.tbl_deep_extend('keep', self.options or {}, default_opts)
 end
 
 function M:update_status()
   local venv = require('venv-selector').get_active_venv()
+  if self.options.auto_hide and vim.bo.filetype ~= 'python' then
+    return ''
+  end
   if venv then
     local venv_parts = vim.fn.split(venv, '/')
+    if system.get_path_separator() == '\\' then
+      venv_parts = vim.fn.split(venv_parts[#venv_parts], '\\')
+    end
     local venv_name = venv_parts[#venv_parts]
     return venv_name
   else
